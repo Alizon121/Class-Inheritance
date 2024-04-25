c8f4a7f059503d23a762d02b7c8b947e63c94e78
Andrew <andrew.lizon12@gmail.com>
2024-04-25 16:00:03 -0700

    Student Module with Notes

--- classes/student.js
+++ classes/student.js
@@ -3,7 +3,10 @@ const Person = require('./person');
 // Making a child class named Student that inherits from Person parent class.
 
 class Student extends Person {
+  // Set new variables in the child constructor
 constructor (firstName, lastName, major, GPA) {
+
+  // super function should take in the desired parent instances 
 super(firstName, lastName)
 this.major = major
 this.GPA = GPA
@@ -11,6 +14,7 @@ this.GPA = GPA
 
 static compareGPA(student1, student2) {
 //  Compare GPAs and return name of individual with higher GPA
+// Refer to different students using dot notation
 
 if (student1.GPA> student2.GPA) {
   return `${student1.firstName} ${student1.lastName} has the higher GPA.`
