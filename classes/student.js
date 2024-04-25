7449c1b414a9b59f54dc48328c682f582d7c84d6
Andrew <andrew.lizon12@gmail.com>
2024-04-25 15:55:19 -0700

    Completed Student Class

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,29 @@
 const Person = require('./person');
 
-// Your code here 
+// Making a child class named Student that inherits from Person parent class.
+
+class Student extends Person {
+constructor (firstName, lastName, major, GPA) {
+super(firstName, lastName)
+this.major = major
+this.GPA = GPA
+}
+
+static compareGPA(student1, student2) {
+//  Compare GPAs and return name of individual with higher GPA
+
+if (student1.GPA> student2.GPA) {
+  return `${student1.firstName} ${student1.lastName} has the higher GPA.`
+}
+else if (student2.GPA > student1.GPA) {
+  return `${student2.firstName} ${student2.lastName} has the higher GPA.`
+
+} else {
+  return "Both students have the same GPA"
+}
+}
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
