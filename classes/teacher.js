5c8315d295ff22d56852e2324c0dbe7909cd124d
Andrew <andrew.lizon12@gmail.com>
2024-04-25 17:15:15 -0700

    Need to Pass Last Spec in Tacher

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,28 @@
 const Person = require('./person');
 
-// Your code here 
+class Teacher extends Person {
+// constructor takes in all instances we desire
+constructor (firstName, lastName, subject, yearsOfExperience) {
+// super function takes in all instances from a parent class
+super(firstName, lastName) 
+this.subject = subject
+this.yearsOfExperience = yearsOfExperience
+
+}
+
+// Static method that takes in an array of teacher intances
+static combinedYearsOfExperience (...teachers) {
+  // return the sum of all teachers years of experience
+ return teachers.reduce((acc, teacher) => acc + teacher)
+
+}
+
+}
+
+
+const teacher1 = ("susan", "jones", "biology", 5);
+const teacher2 = ("bobby", "roberts", "math", 15);
+console.log(Teacher.combinedYearsOfExperience(teacher1, teacher2))
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
